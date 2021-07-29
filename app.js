42045c1deb7cb1fa5ad7b5036fc118453ffb8d50
Syeda <zannatulsakina@gmail.com>
2021-07-29 17:11:31 -0400

    fixed errors app.ts

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var createError = require;
-from;
-'http-errors';
+const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var app = express_1.default();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+const app = express_1.default();
+exports.default = app;
 app.set('views', path_1.default.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(morgan_1.default('dev'));
@@ -23,7 +22,7 @@ app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(function (req, res, next) {
-    next(createError(404));
+    next(http_errors_1.default(404));
 });
 app.use(function (err, req, res, next) {
     res.locals.message = err.message;
@@ -31,5 +30,4 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-module.exports = app;
 //# sourceMappingURL=app.js.map
